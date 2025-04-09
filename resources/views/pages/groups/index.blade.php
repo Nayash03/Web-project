a1e3d00f16fead25ae9e9d5e73e76de0a51c7eac
Nayash03 <shayan.cahelo@edu.esiee-it.fr>
2025-04-09 11:00:00 +0200

    Add | Add promotion table migration

--- resources/views/pages/groups/index.blade.php
+++ resources/views/pages/groups/index.blade.php
@@ -3,7 +3,28 @@
         <h1 class="flex items-center gap-1 text-sm font-normal">
             <span class="text-gray-700">
                 {{ __('Groupes') }}
+                
             </span>
         </h1>
+
+
+        <select class="select" name="select">
+            <option value="1">
+                Option 1
+            </option>
+            <option value="2">
+                Option 2
+            </option>
+            <option value="3">
+                Option 3
+            </option>
+        </select>
+
+    
+
+
+
+        
+
     </x-slot>
 </x-app-layout>
